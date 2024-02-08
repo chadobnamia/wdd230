c77b8564f53219c6fdfc8298dea24a117678129c
ChadDev2022 <richardobnamia@gmail.com>
2024-02-09 06:17:20 +0800

    fix javascript function

--- lesson05/scripts/main.js
+++ lesson05/scripts/main.js
@@ -1,6 +1,6 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('#submit');
-const list = document.querySelector('#list');
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
 
 button.addEventListener('click',()=>{
     if(input != ""){
@@ -9,6 +9,7 @@ button.addEventListener('click',()=>{
         li.textContent = input.value;
         deleteButton.textContent = '❌';
         li.append(deleteButton);
+        list.append(li);
         deleteButton.addEventListener('click',()=>{
             list.removeChild(li);
             input.focus;
