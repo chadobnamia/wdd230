6eab57334de09ff30e99ef8dd7d9a68367670849
ChadDev2022 <richardobnamia@gmail.com>
2024-11-23 07:58:24 +0800

    week 3 edits

---
+++ week03/scripts/main.js
@@ -0,0 +1,35 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener('click', () => {
+    if (input.value !== "") { // Check if input is not empty
+        const li = document.createElement('li'); // Create new list item
+        const deleteButton = document.createElement('button'); // Create delete button
+        li.textContent = input.value; // Set text content of li to input value
+        deleteButton.textContent = '❌'; // Set text content of delete button
+        li.append(deleteButton); // Append delete button to li
+        list.append(li); // Append li to list
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li); // Remove li when delete button is clicked
+            input.focus(); // Set focus back to input field
+            input.value = ''; // Clear input field
+        });
+    } else {
+        alert("You didn't have an input."); // Alert if input is empty
+    }
+});
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+  }
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+  } 
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+  }
