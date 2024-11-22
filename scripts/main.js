462f1acd995e44b0cf0b0908eeaf59af77789f46
ChadDev2022 <richardobnamia@gmail.com>
2024-11-23 05:36:53 +0800

    edit week2 improvement

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,8 @@ const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
+	navigation.classList.toggle('show');
+	hamButton.classList.toggle('show');
 });
 
 // 1️⃣ Initialize display element variable
