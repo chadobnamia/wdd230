1de5bd558b6371f6a8ebd9ba5ec741e8566079ba
ChadDev2022 <richardobnamia@gmail.com>
2024-12-08 06:39:35 +0800

    form page

--- scripts/form.js
+++ scripts/form.js
@@ -1,5 +1,7 @@
 const rangevalue = document.getElementById("rangevalue");
 const range = document.getElementById("r");
+const pw1 = document.getElementById("pw1");
+const pw2 = document.getElementById("pw2");
 
 // RANGE event listener
 range.addEventListener('change', displayRatingValue);
@@ -8,5 +10,16 @@ range.addEventListener('input', displayRatingValue);
 function displayRatingValue() {
     rangevalue.textContent = range.value;
 }
-
-document.
+function checkSame() {
+	if (pw1.value !== pw2.value) {
+		message.textContent = "❗Password DO NOT MATCH!";
+		message.style.visibility = "show";
+		pw2.style.backgroundColor = "#fff0f3";
+		pw2.value = "";
+		pw2.focus();
+	} else {
+		message.style.display = "none";
+		pw2.style.backgroundColor = "#fff";
+		pw2.style.color = "#000";
+	}
+}
